0494073ea96e2e7b6015abbcbfeec09f98dca956
kimdoyeon <lemon8818@naver.com>
2024-05-12 15:59:25 +0900

    #12 [도연] 5월 1주 문제_예인

--- src/main/java/com/study01/answer/yeinQuestionAnswer/DoyeonAnswer.java
+++ src/main/java/com/study01/answer/yeinQuestionAnswer/DoyeonAnswer.java
@@ -1,4 +1,14 @@
 package com.study01.answer.yeinQuestionAnswer;
 
+import com.study01.answer.yeinQuestionAnswer.doyeonAnswerLotto.DoyeonAnswerLotto;
+
 public class DoyeonAnswer {
+    public static void main(String[] args) {
+
+        DoyeonAnswerLotto answer = new DoyeonAnswerLotto();
+        String result = answer.lottoNumber();
+
+        System.out.println(result);
+
+    }
 }
