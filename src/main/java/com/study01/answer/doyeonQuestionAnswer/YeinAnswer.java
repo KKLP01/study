7d6b1fb139348f61fdf708055722fc95f43c435c
brightyein <129862727+brightyein@users.noreply.github.com>
2024-05-12 12:07:51 +0900

    [예인] 팀원들 answer 패키지 생성

--- src/main/java/com/study01/answer/doyeonQuestionAnswer/YeinAnswer.java
+++
@@ -1,4 +0,0 @@
-package com.study01.answer.doyeonQuestionAnswer;
-
-public class YeinAnswer {
-}
---
+++ src/main/java/com/study01/answer/doyeonQuestionAnswer/yein/YeinAnswer.java
@@ -0,0 +1,4 @@
+package com.study01.answer.doyeonQuestionAnswer.yein;
+
+public class YeinAnswer {
+}
