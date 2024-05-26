77c7dd08241f8b66978c88ebca387314fbcf82ac
brightyein <129862727+brightyein@users.noreply.github.com>
2024-05-26 09:16:52 +0900

    #38 [예인] 오목게임 만들기 완료

--- study03-Gomoku/src/main/java/com/KKLP/Answer/yein/Application.java
+++ study03-Gomoku/src/main/java/com/KKLP/Answer/yein/Application.java
@@ -3,7 +3,6 @@ package main.java.com.KKLP.Answer.yein;
 public class Application {
 
     public static void main(String[] args) {
-
         PlayerAction playerAction = new PlayerAction();
         CheckGame checkGame = new CheckGame();
 
@@ -13,12 +12,33 @@ public class Application {
         // 플레이어 핑퐁하기
         while (true) {
 
-            checkGame.checkHorizon(playerAction.player1Input());
-            checkGame.checkHorizon(playerAction.player2Input());
-
-            // 이곳에 승리를 판별하는 메서드 작성할 것
-            // true : 아직 승부 판별 못하는 상태
+            boolean isDone;
+            // true : 아직 승부 판별 불가
             // false : 승부가 끝난 상태
+
+            // player1 돌 놓기
+            String[][] afterPlayer1 = playerAction.player1Input();
+
+            // 승부 검증
+            isDone = checkGame.checkHorizon(afterPlayer1);
+            if (isDone == false) break;
+            isDone = checkGame.checkVertical(afterPlayer1);
+            if (isDone == false) break;
+            isDone = checkGame.checkCross(afterPlayer1);
+            if (isDone == false) break;
+
+            // player2 돌 놓기
+            String[][] afterPlayer2 = playerAction.player2Input();
+
+            // 승부 검증
+            isDone = checkGame.checkHorizon(afterPlayer2);
+            if (isDone == false) break;
+            isDone = checkGame.checkVertical(afterPlayer2);
+            if (isDone == false) break;
+            isDone = checkGame.checkCross(afterPlayer2);
+            if (isDone == false) break;
+
+
         }
     }
 }
--- study03-Gomoku/src/main/java/com/KKLP/Answer/yein/PlayerAction.java
+++ study03-Gomoku/src/main/java/com/KKLP/Answer/yein/PlayerAction.java
@@ -4,21 +4,25 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class PlayerAction {
-    CheckGame checkGame = new CheckGame();
 
-    public static String[][] gumoku = new String[10][10];
-    int player1Count = 0; // 몇번째 두는 돌인지 체크
-    public static int[][] player1 = new int[10][2];
+    public static String[][] gumoku = new String[11][11];
 
-    // 시작 오목판 출력
+    // 시작 오목판 세팅
     public void startGomoku() {
-        for (int i = 0; i < gumoku.length; i++) {
-            for (int j = 0; j < gumoku.length; j++) {
-                gumoku[i][j] = "[ ]";
-                System.out.print(gumoku[i][j] + " ");
-            }
-            System.out.println();
+
+        gumoku[0][0] = "   ";
+
+        for (int i = 1; i < gumoku.length; i++) {
+            gumoku[0][i] = String.valueOf(i-1)+"  ";
+            gumoku[i][0] = String.valueOf(i-1)+" ";
         }
+
+        for (int i = 0; i < gumoku.length-1; i++) {
+            for (int j = 0; j < gumoku.length-1; j++) {
+                gumoku[i+1][j+1] = "[ ]";
+            }
+        }
+        currentGomoku();
     }
 
     // 현재 오목판 출력
@@ -39,24 +43,14 @@ public class PlayerAction {
 
             System.out.println("player1 의 차례입니다.");
             System.out.println("몇 행을 선택할까요? : ");
-            int player1FirstNum = sc.nextInt();
+            int player1FirstNum = sc.nextInt()+1;
             System.out.println("몇 열을 선택할까요? : ");
-            int player1SecondNum = sc.nextInt();
+            int player1SecondNum = sc.nextInt()+1;
 
             if (gumoku[player1FirstNum][player1SecondNum].equals("[ ]")) { // 빈 자리라면
 
-                System.out.println(player1FirstNum + "행 " + player1SecondNum + "열에 돌을 둡니다.");
+                System.out.println((player1FirstNum-1) + "행 " + (player1SecondNum-1) + "열에 돌을 둡니다.");
                 gumoku[player1FirstNum][player1SecondNum] = "[O]"; // 돌을 둔다
-
-                // 몇행 몇열에 뒀는지 배열에 저장 후 출력
-                player1[player1Count][0] = player1FirstNum;
-                player1[player1Count][1] = player1SecondNum;
-                player1Count++;
-
-                for (int[] ints : player1) {
-                    System.out.print(Arrays.toString(ints));
-                }
-                System.out.println();
 
                 // 돌을 둔 후 현재 오목판 출력
                 currentGomoku();
@@ -74,12 +68,12 @@ public class PlayerAction {
 
             System.out.println("player2 의 차례입니다.");
             System.out.println("몇 행을 선택할까요? : ");
-            int player2FirstNum = sc.nextInt();
+            int player2FirstNum = sc.nextInt()+1;
             System.out.println("몇 열을 선택할까요? : ");
-            int player2SecondNum = sc.nextInt();
+            int player2SecondNum = sc.nextInt()+1;
 
             if (gumoku[player2FirstNum][player2SecondNum].equals("[ ]")) {
-                System.out.println(player2FirstNum + "행 " + player2SecondNum + "열에 돌을 둡니다.");
+                System.out.println((player2FirstNum-1) + "행 " + (player2SecondNum-1) + "열에 돌을 둡니다.");
                 gumoku[player2FirstNum][player2SecondNum] = "[X]";
 
                 currentGomoku();
